e45b12a8794565e318cecf90dcbb97797ce7caae
ZJRamsey <u1756073@unimail.hud.ac.uk>
2019-10-22 16:48:21 +0100

    week3 swap added, some changes

---
+++ week3/tests/genericMethods/SwapTest.java
@@ -0,0 +1,15 @@
+package genericMethods;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SwapTest {
+
+    @Test
+    void testInt()
+    {
+        int testArray[];
+        //testArray = [5,7,3,2,0,66];
+    }
+}
