18ce023832806e2acd55069240a3d5e43a749191
ZJRamsey <u1756073@unimail.hud.ac.uk>
2019-11-19 16:55:44 +0000

    Added week 7 and singlelinklist

---
+++ week7/src/linkedList/list/SingleLinkList.java
@@ -0,0 +1,21 @@
+package linkedList.list;
+
+import linkedList.node.SingleLinkNode;
+
+public class SingleLinkList<T> extends BasicList<SingleLinkNode<T>, T> implements List<T>
+{
+    @Override
+    public void add(int index, T value) throws ListAccessError {
+
+    }
+
+    @Override
+    public T remove(int index) throws ListAccessError {
+        return null;
+    }
+
+    @Override
+    public T get(int index) throws ListAccessError {
+        return null;
+    }
+}
