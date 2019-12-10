8c70a839d7afe2ca8b791a3f816187737e404d4d
ZJRamsey <u1756073@unimail.hud.ac.uk>
2019-12-10 15:31:00 +0000

    Finished suite of tests for sorts

---
+++ week10/src/graph/DepthFirstTraversal.java
@@ -0,0 +1,12 @@
+package graph;
+
+import java.util.List;
+
+public class DepthFirstTraversal<T> implements Traversal<T>
+{
+
+    @Override
+    public List<T> traverse() throws GraphError {
+        return null;
+    }
+}
---
+++ week5/tests/arraySorter/IntegerQuickSortTest.java
@@ -0,0 +1,10 @@
+package arraySorter;
+
+import arrayGenerator.ArrayGenerator;
+import arrayGenerator.IntegerArrayGenerator;
+import scope.IntegerScope;
+
+public class IntegerQuickSortTest extends QuickSortTest<Integer> {
+    @Override
+    ArrayGenerator<Integer> getGenerator() { return new IntegerArrayGenerator(new IntegerScope());}
+}
