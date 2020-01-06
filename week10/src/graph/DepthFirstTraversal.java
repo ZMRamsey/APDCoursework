c16984a8518651c11fa7c45c97b1f4dcfff626a1
Zoey <zoey.ramsey1@gmail.com>
2020-01-06 16:39:30 +0000

    Added Depth First Graph Traversal

--- week10/src/graph/DepthFirstTraversal.java
+++ week10/src/graph/DepthFirstTraversal.java
@@ -1,15 +1,65 @@
 package graph;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class DepthFirstTraversal<T> extends AdjacencyGraph<T> implements Traversal <T>
 {
+    private AdjacencyGraph<T> graph;
+
     @Override
     public List<T> traverse() throws GraphError {
 
-        //traverse and stuff
+        if (graph == null){throw new GraphError();}
+
+        List<T> traversalList = new ArrayList<>();
+
+        Set<T> NodeSet = graph.getNodes();
+        Iterator<T> nodeIterator = NodeSet.iterator();
+
+        while (nodeIterator.hasNext())
+        {
+            T node = nodeIterator.next();
+            if (!(checkList(traversalList, node)))
+            {
+                traversalList = visit(node, traversalList);
+            }
+        }
+
+        return traversalList;
+    }
+
+    public List<T> visit(T node, List<T> list) throws GraphError {
+        list.add(node);
+
+        Set<T> neighbours = graph.getNeighbours(node);
+        Iterator<T> neighbourator = neighbours.iterator();
+
+        while (neighbourator.hasNext()){
+            list = visit(neighbourator.next(), list);
+        }
 
 
-        return null;
+        return list;
+    }
+
+    public void setGraph(AdjacencyGraph<T> newGraph)
+    {
+        graph = newGraph;
+    }
+
+    public boolean checkList(List<T> list, T value)
+    {
+        Iterator<T> iterator = list.iterator();
+        while (iterator.hasNext())
+        {
+            if (iterator.next() == value)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
