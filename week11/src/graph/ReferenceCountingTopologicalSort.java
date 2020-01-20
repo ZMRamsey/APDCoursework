1d2bd4518ec2acd5258ee4db4620bbbd0127bbba
ZJRamsey <u1756073@unimail.hud.ac.uk>
2020-01-20 22:22:59 +0000

    finished week 11

--- week11/src/graph/ReferenceCountingTopologicalSort.java
+++ week11/src/graph/ReferenceCountingTopologicalSort.java
@@ -1,26 +1,83 @@
 package graph;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 public class ReferenceCountingTopologicalSort<T> extends AdjacencyGraph<T> implements TopologicalSort<T>
 {
 
     Stack<T> sort;
-    Set<T> visiting = new HashSet<T>();
-
-    //while(there are nodes left in the graph)
-    //  find a node with no predecessors;
-    //  add it after any previously sorted nodes;
-    //  remove it from the graph;
+    Stack<T> checklist;
+    T[] nodeArray;
+    T[][] neighbourArray;
 
     @Override
     public List<T> getSort() throws GraphError {
 
         sort = new Stack<T>();
 
+        int i = 0;
+        for (T node: getNodes())
+        {
+            nodeArray[i] = node;
+            i++;
+        }
 
+        while (nodeArray.length > 0) { Visit(); }
+
+        Collections.reverse(sort);
+        return sort;
+    }
+
+    void Visit() throws GraphError {
+
+        //update references
+        for (int h = 0; h < nodeArray.length; h++)
+        {
+            neighbourArray[h] = (T[]) getNeighbours(nodeArray[h]).toArray();
+        }
+
+        for (int j = 0; j < neighbourArray.length; j++)
+        {
+            for (int k = 0; k < neighbourArray[j].length; k++)
+            {
+                checklist.push(neighbourArray[j][k]);
+            }
+        }
+
+        while (checklist.peek() != null)
+        {
+            T testNode = checkArray((T[]) checklist.toArray());
+
+            sort.push(testNode);
+            T[] newArray = null;
+            for (int l = 0; l < nodeArray.length; l++)
+            {
+                if (nodeArray[l] != testNode) { newArray[l] = nodeArray[l]; }
+            }
+            nodeArray = newArray;
+            break;
+        }
+    }
+
+    T checkArray(T[] array)
+    {
+        for (int x = 0; x < nodeArray.length; x++)
+        {
+            boolean isThere = false;
+            for (int z = 0; z < array.length; z++)
+            {
+                if (array[z] == nodeArray[x])
+                {
+                    isThere = true;
+                    break;
+                }
+            }
+
+            if (!isThere)
+            {
+                return nodeArray[x];
+            }
+        }
+        return null;
     }
 }
