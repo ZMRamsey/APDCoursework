9153a2831a286a3622ddddd9f9e4c49576299761
ZJRamsey <u1756073@unimail.hud.ac.uk>
2020-02-04 15:08:44 +0000

    Added week 14

---
+++ week14/src/ferrocarrilesDeAmericaDelSur/railways/Bolivia.java
@@ -0,0 +1,36 @@
+package ferrocarrilesDeAmericaDelSur.railways;
+
+import ferrocarrilesDeAmericaDelSur.errors.RailwaySystemError;
+import ferrocarrilesDeAmericaDelSur.errors.SetUpError;
+import ferrocarrilesDeAmericaDelSur.tools.Clock;
+import ferrocarrilesDeAmericaDelSur.tools.Delay;
+
+/**
+ * An implementation of a railway.  The runTrain method, should, in collaboration with Peru's runTrain(), guarantee
+ * safe joint operation of the railways.
+ */
+public class Bolivia extends Railway {
+	/**
+     * Change the parameters of the Delay constructor in the call of the superconstructor to
+	 * change the behaviour of this railway.
+	 * @throws SetUpError if there is an error in setting up the delay.
+	 */
+	public Bolivia() throws SetUpError {
+		super("Bolivia",new Delay(0.1,0.3));
+	}
+
+    /**
+     * Run the train on the railway.
+     * This method currently does not provide any synchronisation to avoid two trains being in the pass at the same time.
+     */
+    public void runTrain() throws RailwaySystemError {
+    	Clock clock = getRailwaySystem().getClock();
+    	while (!clock.timeOut()) {
+    		choochoo();
+			while (getSharedBasket().hasStone());
+			getSharedBasket().putStone();
+			crossPass();
+			getSharedBasket().takeStone();
+    	}
+    }
+}
