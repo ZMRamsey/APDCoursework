8859758b784eddb432fae6e0e707cd85ee4dd731
ZJRamsey <u1756073@unimail.hud.ac.uk>
2020-04-29 15:09:25 +0100

    Final upload

--- week23/src/arithmetic/Cube.java
+++ week23/src/arithmetic/Cube.java
@@ -5,40 +5,71 @@ import javax.swing.JOptionPane;
 /**
  * Defines a method for calculating the cube of an integer.  The integer value to be cubed is input by the user.
  *
- * @version March 2020
  * @author Hugh Osborne
+ * @version March 2020
  */
 public class Cube {
-	/**
-	 * Cube an integer.
-	 *
-	 * This method should be provided with a proof.
-	 *
-	 * @param n the number to be cubed.
-	 * @return n cubed.
-	 */
-	public static int cube(int n) {
-		  int cube = 0, threeNsquared = 0, threeN = 0;
+    /**
+     * Cube an integer.
+     * <p>
+     * This method should be provided with a proof.
+     *
+     * @param n the number to be cubed.
+     * @return n cubed.
+     */
+    public static int cube(int n) {
+        int cube = 0, threeNsquared = 0, threeN = 0;
+        int i = 0;
 
-		  int i = 0;
-		  while (i < n) {
-			  cube = cube + threeNsquared + threeN + 1;
-			  threeNsquared = threeNsquared + 2*threeN + 3;
-			  threeN = threeN + 3;
-			  i++;
-		  }
-		  return cube;
-	}
+        assert cube == i*i*i : cube;
+        assert threeN == 3*i : threeN;
+        assert threeNsquared == 3*i*i : threeNsquared;
 
-	/**
-	 * Run the cube method.
-	 *
-	 * @param args (ignored)
-	 */
-	public static void main(String[] args) {
-		int x = ReadInt.read("Please input a number to be squared");
-		int result = cube(x);
-		JOptionPane.showMessageDialog(null,x + "^3=" + result,"Function result",JOptionPane.INFORMATION_MESSAGE);
-	}
+        while (i < n) {
+
+            assert cube == i*i*i : cube;
+            assert threeN == 3*i : threeN;
+            assert threeNsquared == 3*i*i : threeNsquared;
+
+            cube = cube + threeNsquared + threeN + 1;
+
+            assert cube == (i+1)*(i+1)*(i+1);
+            assert threeN == 3*i;
+            assert threeNsquared == 3*i*i;
+
+            threeNsquared = threeNsquared + (2*threeN) + 3;
+
+            assert cube == (i+1)*(i+1)*(i+1);
+            assert threeN == 3*i;
+            assert threeNsquared == 3*(i+1)*(i+1);
+
+            threeN = threeN + 3;
+
+            assert cube == (i+1)*(i+1)*(i+1);
+            assert threeN == 3*(i+1);
+            assert threeNsquared == 3*(i+1)*(i+1);
+
+            i++;
+
+            assert(cube == i*i*i);
+            assert(threeN == 3*i);
+            assert(threeNsquared == 3*i*i);
+        }
+        assert(cube == n*n*n);
+        assert(threeN == 3*n);
+
+        return cube;
+    }
+
+    /**
+     * Run the cube method.
+     *
+     * @param args (ignored)
+     */
+    public static void main(String[] args) {
+        int x = ReadInt.read("Please input a number to be cubed");
+        int result = cube(x);
+        JOptionPane.showMessageDialog(null, x + "^3=" + result, "Function result", JOptionPane.INFORMATION_MESSAGE);
+    }
 
 }
--- week23/src/arithmetic/Square.java
+++ week23/src/arithmetic/Square.java
@@ -25,23 +25,33 @@ public class Square {
 	 */
 	public static int square(int n) {
 		  int i = 0, square = 0, twoN = 0;
+
 	      assert square == i*i : square;
 	      assert twoN == 2*i : twoN;
+
 		  while (i < n) {
+
 		      assert square == i*i : square;
 		      assert twoN == 2*i : twoN;
+
 		      square = square + twoN + 1;
+
 		      assert square == (i+1)*(i+1);
 		      assert twoN == 2*i;
+
 		      twoN = twoN + 2;
+
 		      assert(square == (i+1)*(i+1));
 		      assert(twoN == 2*(i + 1));
+
 		      i++;
+
 		      assert(square == i*i);
 		      assert(twoN == 2*i);
 		  }
 		   assert square == n*n : square;
 		   assert twoN == 2*n;
+
 		   return square;
 	}
 
